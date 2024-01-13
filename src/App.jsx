d2c3330f9e241278c91c9313076e0ac10eaab893
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-13 21:52:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { Container } from "./views/Container/Container"
+import { Footer } from "./views/Footer/Footer"
 import { Header } from "./views/Header/Header"
+import { Main } from "./views/Main/Main"
 
 const App = () => (
   <>
-    <Header>hed</Header>
-    <h1>Заголовок веб приложения</h1>
-    <div className="divv">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor tempora, blanditiis expedita rerum </p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, incidunt.</p>
-    </div>
+    <Header />
+
+    <Main>
+      <Container>
+        <h1>Заголовок веб приложения</h1>
+      </Container>
+      <Container>
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, incidunt.</p>
+        </div>
+      </Container>
+    </Main>
+
+    <Footer />
   </>
 )
 
