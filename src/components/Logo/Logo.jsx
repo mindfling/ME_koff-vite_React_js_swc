07dad7e7f41d19d013daaebbb74c8d39760fc2ed
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-14 08:15:51 +0300

    feat logo try

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,6 +3,11 @@ import s from './Logo.module.scss';
 
 export const Logo = () => (
   <div className={s.logo}>
-    <img src={logosvg} alt="logo lgoo" className="logo" />
+    <a className="header__link-logo" href="/" >
+      <img className="header__logo" src="/logo-koff.svg" alt="Логотип мебельного маркета Koff" title='Главная страница мебельного маркета Koff'/>
+    </a>
+    <img src={logosvg} alt="Логотип мебельного маркета Koff" />
   </div>
 )
+
+
