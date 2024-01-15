83fe547e605c7bc9045c5bbfd01c5915a91dfb75
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2024-01-15 23:17:57 +0300

    Temp update root

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import 'normalize.css'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import 'normalize.css';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootSelector = '#root';
+const rootElement = document.querySelector(rootSelector);
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>,
+);
