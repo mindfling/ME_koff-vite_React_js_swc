1c9d9782325b2f5da0cb94136b3677a9ff0d272c
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2024-01-15 23:17:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Container } from "./views/Container/Container"
-import { Footer } from "./views/Footer/Footer"
-import { Header } from "./views/Header/Header"
-import { Main } from "./views/Main/Main"
+import { Container } from './views/Container/Container';
+import { Footer } from './views/Footer/Footer';
+import { Header } from './views/Header/Header';
+import { Main } from './views/Main/Main';
 
 const App = () => (
   <>
@@ -12,14 +12,19 @@ const App = () => (
         <h1>Заголовок веб приложения</h1>
       </Container>
       <Container>
-        <div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, incidunt.</p>
-        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde suscipit eligendi beatae consectetur dolorem,
+          quae at nemo quis sequi velit in? Assumenda debitis ratione placeat cumque asperiores?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos possimus facere repudiandae qui eaque amet ut
+          harum consectetur vitae eius impedit tempora earum nobis aspernatur, reiciendis adipisci
+        </p>
       </Container>
     </Main>
 
     <Footer />
   </>
-)
+);
 
-export default App
+export default App;
