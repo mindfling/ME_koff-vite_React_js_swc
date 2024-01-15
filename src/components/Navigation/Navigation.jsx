d4e84f7529349a8c3123436240399e152695ef8c
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-15 08:26:14 +0300

    feat modify

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,15 @@
 import s from './Navigation.module.scss';
-const numberGoods = 1;
+const numberGoods = 1; // * debug
 
 export const Navigation = () => (
   <nav className={s.navigation}>
-    <a className={s.link} href="/favorite"><span className={s.linkText}>Избранное</span>
+    <a className={s.link} href="/favorite"  title='Перейти в Избранное'>
+      <span className={s.linkText}>Избранное</span>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
         <path d="M8.41331 13.8733C8.18665 13.9533 7.81331 13.9533 7.58665 13.8733C5.65331 13.2133 1.33331 10.46 1.33331 5.79332C1.33331 3.73332 2.99331 2.06665 5.03998 2.06665C6.25331 2.06665 7.32665 2.65332 7.99998 3.55998C8.67331 2.65332 9.75331 2.06665 10.96 2.06665C13.0066 2.06665 14.6666 3.73332 14.6666 5.79332C14.6666 10.46 10.3466 13.2133 8.41331 13.8733Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
       </svg>
     </a>
-    <a className={s.link} href="/cart">
+    <a className={s.link} href="/cart" title='Перейти в Корзину'>
       <span className={s.linkText}>Корзина</span>
       <span className={s.count}>({numberGoods})</span>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
