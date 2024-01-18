5b5f4de9c58abab2ef34d3afb6e11e9decc748f2
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2024-01-18 23:40:12 +0300

    feat modern-normalize at index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import 'modern-normalize';
 import './index.scss';
 
 const rootSelector = '#root';
