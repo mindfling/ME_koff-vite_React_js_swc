a089e9a28f7a67036198efd3a859818f15b34556
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-20 01:25:16 +0300

    feat goods

--- src/views/Goods/Goods.jsx
+++ src/views/Goods/Goods.jsx
@@ -1,25 +1,38 @@
 // * Goods
+import { CardItem } from '../CardItem/CardItem';
+import { Container } from '../Container/Container';
+import s from './Goods.module.scss';
+
 
 export const Goods = () => {
-  
+
   return (
-    <Container>
-    <div className="goods">
-      <ul className="goods__list">
+    <div className={s.goods}>
+      <Container>
+        <ul className={s.list}>
 
-        <li className="goods__item">
-          <article className='card'>
-            <img src="" alt="" className="card__image" />
-            <div className="card__info">
-              <span className='card__description'></span>
-              <span className="card__price"></span>
-            </div>
-            <button className="card__btn"></button>
-          </article>
-        </li>
+          <li className={s.item}>
+            <CardItem />
+          </li>
 
-      </ul>
+          <li className={s.item}>
+            <CardItem />
+          </li>
+
+          <li className={s.item}>
+            <CardItem />
+          </li>
+
+          <li className={s.item}>
+            <CardItem />
+          </li>
+
+          <li className={s.item}>
+            <CardItem />
+          </li>
+
+        </ul>
+      </Container>
     </div>
-  </Container>
   )
 }
