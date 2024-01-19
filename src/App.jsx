7b7ab409fa72a843f1998d47a9aa497c673b70f4
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2024-01-19 16:25:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,9 @@ import { Main } from './views/Main/Main';
 const App = () => (
   <>
     <Header />
-
-    <Main>
-
-      <Container>
-        <h1>Список веб товаров</h1>
-        <div className="catalogue">
-          <ul className="catalogue__list">
-            <li className="catalogue__item">
-              <a href="#" className="catalogue__link">Тумбы</a>
-            </li>
-            <li className="catalogue__item">
-              <a href="#" className="catalogue__link">Столы</a>
-            </li>
-          </ul>
-        </div>
-        <p className='visuall-hidden'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ex expedita officiis et, esse quidem, consectetur, quas aut soluta commodi sed! Molestias quam, corporis impedit magni dolorem consequuntur explicabo quae.</p>
-      </Container>
-
-      <Container>
-        <div className="goods">
-          <ul className="goods__list">
-
-            <li className="goods__item">
-              <article className='card'>
-                <img src="" alt="" className="card__image" />
-                <div className="card__info">
-                  <span className='card__description'></span>
-                  <span className="card__price"></span>
-                </div>
-                <button className="card__btn"></button>
-              </article>
-            </li>
-
-          </ul>
-        </div>
-      </Container>
-
-    </Main>
-
+    
+    <Main />
+    
     <Footer />
   </>
 );
