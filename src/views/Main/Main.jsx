077733a40553eee8a9c5d712ec8182454d679844
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-20 00:19:14 +0300

    feat fix Main

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,14 +1,12 @@
 // * main
+import {Catalogue} from '../Catalog/Catalog';
+import {Goods} from '../Goods/Goods';
 import s from './Main.module.scss';
 
 export const Main = () => (
   <main className={s.main}>
-    <Container>
-      {/* Catalogue */}
-    </Container>
-    <Container>
-      {/* Goods */}
-    </Container>
+      <Catalogue />
+      <Goods />
   </main>
 )
 
