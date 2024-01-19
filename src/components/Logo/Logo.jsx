41c60497a55b4a57fe3bdd962e4bbb8e6cc97dc2
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-19 15:32:14 +0300

    feat Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,6 +4,11 @@ import s from './Logo.module.scss';
 
 export const Logo = (props) => (
   <a className={props.className ? `${props.className} ${s.logo}` : s.logo} href="/" >
-    <img className={s.img} src={logosvg} alt="Логотип мебельного маркета Koff" title='Главная страница мебельного маркета Koff' />
+    <img
+      className={s.img}
+      src={logosvg}
+      alt="Логотип мебельного маркета Koff"
+      title='Главная страница мебельного маркета Koff'
+    />
   </a>
 )
