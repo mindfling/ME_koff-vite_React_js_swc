7758d3a96c11c59ad01e490fcdf91e483e8659d9
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2024-01-20 19:24:28 +0300

    feat app dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { Container } from './views/Container/Container';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Footer } from './views/Footer/Footer';
 import { Header } from './views/Header/Header';
 import { Main } from './views/Main/Main';
+import { useEffect } from 'react';
+import { fetchAccessToken } from './store/auth/authSlice';
 
-const App = () => (
+const App = () => {
+  const dispatch = useDispatch();
+  const { accessToken, loading } = useSelector(state => state.auth);
+  
+  useEffect(() => {
+    dispatch(fetchAccessToken())
+  }, [dispatch, accessToken]);
+  
+  
+return (
   <>
     <Header />
-    
     <Main />
-    
     <Footer />
   </>
-);
+)
+}
 
 export default App;
