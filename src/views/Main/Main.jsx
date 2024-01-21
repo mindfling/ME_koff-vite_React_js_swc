68f3b9804d2f0cd7d5003d6de4a6affe297d5069
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2024-01-21 19:22:24 +0300

    feat Main

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,12 +1,30 @@
 // * main
-import {Catalog} from '../Catalog/Catalog';
-import {Goods} from '../Goods/Goods';
 import s from './Main.module.scss';
+import { Catalog } from '../Catalog/Catalog';
+import { Goods } from '../Goods/Goods';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCategories } from '../../store/categories/categoriesSlice';
 
-export const Main = () => (
-  <main className={s.main}>
-      <Catalog />
+export const Main = () => {
+  const dispatch = useDispatch();
+  const {
+    data: dataCategories,
+    loading: loadingCategories,
+    error: errorCategories,
+  } = useSelector((state) => state.categories);
+  
+  useEffect(() => {
+    console.log('dataCategories: ', dataCategories);
+    console.log('loadingCategories: ', loadingCategories);
+    console.log('errorCategories: ', errorCategories);
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  return (
+    <main className={s.main}>
+      <Catalog data={dataCategories}/>
       <Goods />
-  </main>
-)
-
+    </main>
+  );
+}
