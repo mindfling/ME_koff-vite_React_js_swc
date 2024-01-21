b4d3756070b7d986767624f5a1eca7f154ed8723
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2024-01-21 16:40:43 +0300

    Create categoriesSlice.js

---
+++ src/store/categories/categoriesSlice.js
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+
+// const API_KEY = 'https://koff-api.vercel.app/api/users/accessKey';
+
+export const fetchCategories = createAsyncThunk(
+  'categories/fetchCategories',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const token = state.auth.accessToken;
+    
+    const response = await fetch('https://koff-api.vercel.app/api/productCategories', {
+      headers: {
+        'Authorisation': `Bearer ${token}`,
+      }
+    })
+  }
+)
+
+
+const initialState = {
+  data: [],
+  loading: false,
+  error: null,
+}
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(fetchCategories.error, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+  },
+});
