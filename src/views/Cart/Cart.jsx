28233dd16ca27086e42e7b80a33cc1022f31980b
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-26 22:42:16 +0300

    feat cart styles

---
+++ src/views/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+import { Container } from '../Container/Container';
+import s from './Cart.module.scss';
+
+
+export const Cart = () => (
+  <>
+    <Container>
+      <div>Корзина с моими Заказами</div>
+    </Container>
+  </>
+);
