ec96418eef761e4898982dec2fc5539622af64ab
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-26 22:49:32 +0300

    feat carditem

--- src/views/CardItem/CardItem.jsx
+++ src/views/CardItem/CardItem.jsx
@@ -2,6 +2,7 @@
 import { API_URL } from '../../const';
 import s from './CardItem.module.scss';
 
+// тестовый товар
 const testProduct = {
   name: 'Кресло Подлокотниками',
   image: '/img/photo.jpg',
@@ -10,6 +11,11 @@ const testProduct = {
 };
 
 export const CardItem = ({ name, images: [image], price, id }) => {
+  const handleFavories = () => {
+    alert(`add product ${name} to favorites`);
+    console.log(`add product ${name} to favorites`);
+  }
+  
   return (
     <article className={s.card} title={`Товар ${name}`}>
       <a href={`/product/${id}`} className={s.link}>
@@ -26,11 +32,10 @@ export const CardItem = ({ name, images: [image], price, id }) => {
         </p>
       </div>
       <button className={s.btn} title={'Добавить ' + name + ' в Корзину'}>В Корзину</button>
-      <button className={s.favorite} title='Like ta Favorites'>
+      <button className={s.favorite} onClick={handleFavories} title='Like ta Favorites'>
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M8.41325 13.8733C8.18658 13.9533 7.81325 13.9533 7.58659 13.8733C5.65325 13.2133 1.33325 10.46 1.33325 5.79332C1.33325 3.73332 2.99325 2.06665 5.03992 2.06665C6.25325 2.06665 7.32658 2.65332 7.99992 3.55998C8.67325 2.65332 9.75325 2.06665 10.9599 2.06665C13.0066 2.06665 14.6666 3.73332 14.6666 5.79332C14.6666 10.46 10.3466 13.2133 8.41325 13.8733Z"
-            fill="white"
             stroke="#1C1C1C"
             strokeLinecap="round"
             strokeLinejoin="round"
