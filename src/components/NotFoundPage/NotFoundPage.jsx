77eb2882f4ec8ea35dfbb6d38dff115b21d0e8b0
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-28 10:39:11 +0300

    feat import 404 styles from my work

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,19 @@
 import { Container } from "../../views/Container/Container";
+import './NotFoundPage.scss';
 
 export const NotFoundPage = () => (
   <Container>
-    <h1>Page 404 ;)</h1>
+    <div className="error-page404 page">
+
+      <p>&nbsp;</p>
+      <h1>Хакеры балуются, что ли?</h1>
+      <h3>Страница не найдена</h3>
+      <p>Ошибка номер <b>404</b>, в основном появляется потому что данная страницу не была найдена на нашем сайте...
+        или из-за чего-то еще...
+      </p><p>
+        <a href="/">Перейти на главную страницу</a></p>
+      <p></p>
+      <p>&nbsp;</p>
+    </div>
   </Container>
 )
