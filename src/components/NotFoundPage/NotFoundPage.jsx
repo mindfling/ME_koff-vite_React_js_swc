0d7162aa9a0f58a9da3a389c6df548f2399d2351
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-28 16:49:56 +0300

    feat not found page styles

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,19 +1,15 @@
 import { Container } from "../../views/Container/Container";
-import './NotFoundPage.scss';
+// import './NotFoundPage.scss';
+import s from './NotFoundPage.module.scss';
 
 export const NotFoundPage = () => (
   <Container>
-    <div className="error-page404 page">
-
-      <p>&nbsp;</p>
-      <h1>Хакеры балуются, что ли?</h1>
-      <h3>Страница не найдена</h3>
-      <p>Ошибка номер <b>404</b>, в основном появляется потому что данная страницу не была найдена на нашем сайте...
-        или из-за чего-то еще...
-      </p><p>
-        <a href="/">Перейти на главную страницу</a></p>
-      <p></p>
-      <p>&nbsp;</p>
+    <div className="error__not-found">
+      <h1 className="error__title">Хакеры балуются, что ли?</h1>
+      <h3 className="error__subtitle">Страница не найдена ;(</h3>
+      <p className="error__text">Ошибка номер <b>404</b>, в основном появляется потому что данная страницу не была найдена на нашем сайте...
+        или из-за чего-то еще...</p>
+      <p className="error__text" title="Переход на главную страницу сайта"><a href="/">Перейти на главную страницу</a></p>
     </div>
   </Container>
 )
