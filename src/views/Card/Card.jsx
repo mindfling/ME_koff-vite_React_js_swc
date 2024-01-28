4186fee62160eaff1f1c5de4c4f1c86e1f79c12b
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-28 10:27:06 +0300

    feat Card is not a carditem

---
+++ src/views/Card/Card.jsx
@@ -0,0 +1,14 @@
+import { Container } from "../Container/Container"
+
+
+export const Card = () => {
+
+  return (
+    <>
+      <Container>
+        <p>Отдельная страница товара</p>
+        <p>Товар артику ...</p>
+      </Container>
+    </>
+  )
+}
