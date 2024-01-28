15ab72b4f6c25a10b297c47304bf6e31e3ca12c5
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-29 02:42:59 +0300

    Update const.js

--- src/const.js
+++ src/const.js
@@ -1,3 +1,4 @@
+export const SITE_TITLE = 'Koff';
 export const STORAGE_KEY = 'accessToken';
 export const API_URL = 'https://koff-api.vercel.app';
 export const API_ACCESSKEY = `${API_URL}/api/users/accessKey`;
