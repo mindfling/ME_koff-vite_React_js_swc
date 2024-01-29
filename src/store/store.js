bf7cae872dc344a07b0458d563631c5cdb9e4544
Nepochtov Dmitry <dimendroider@gmail.com>
2024-01-30 02:14:43 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
 import categoriesReducer from './categories/categoriesSlice';
 import productsReducer from './products/productsSlice';
+import productReducer from './product/productSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     categories: categoriesReducer,
     products: productsReducer,
+    product: productReducer,
   },
-})
+});
